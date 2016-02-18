7a72e8b50cc809209ac61ef0c454264a1740f889
georghinkel <hinkel@fzi.de>
2016-02-18 23:39:29 +0100

    Initial commit of NMF example project

---
+++ FSM2PN.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Linq;
+using NMF.Transformations;
+using NMF.Transformations.Core;
+using NMF.Utilities;
+using NMFDemo.Metamodels.FSM;
+using PN = NMFDemo.Metamodels.PN;
+
+namespace NMFDemo
+{
+    /// <summary>
+    /// This is an example of a simple model transformation in NTL
+    /// </summary>
+    public class FSM2PN : ReflectiveTransformation
+    {
+        /// <summary>
+        /// The transformation rules are public nested classes. This first transformation rule transforms state machines to Petri nets
+        /// </summary>
+        public class AutomataToNet : TransformationRule<StateMachine, PN.Net>
+        {
+            /// <summary>
+            /// Here we specify how exactly state machines are transformed to Petri nets
+            /// </summary>
+            /// <param name="input">The input state machine</param>
+            /// <param name="output">The target Petri net output</param>
+            /// <param name="context">The context of this transformation run, used e.g. for tracing</param>
+            /// <remarks>We could also override other bits of the transformation such as for example altering the way how target model elements are created</remarks>
+            public override void Transform(StateMachine input, PN.Net output, ITransformationContext context)
+            {
+                output.Name = input.Name;
+            }
+        }
+
+        /// <summary>
+        /// This rule transforms states of the input state machine into Petri net places
+        /// </summary>
+        public class StateToPlace : TransformationRule<IState, PN.Place>
+        {
+            /// <summary>
+            /// During transformation, we simply copy the name
+            /// </summary>
+            /// <param name="input">The input model element</param>
+            /// <param name="output">The target Petri net place</param>
+            /// <param name="context">The context of this transformation run, used e.g. for tracing</param>
+            public override void Transform(IState input, PN.Place output, ITransformationContext context)
+            {
+                output.Name = input.Name;
+            }
+
+            /// <summary>
+            /// In this RegisterDependencies method, we may add dependencies to other transformation rules
+            /// </summary>
+            public override void RegisterDependencies()
+            {
+                // The transformation rule State2Place should be called each time we transform a finite state machine to a Petri net
+                CallForEach(Rule<AutomataToNet>(),
+                    selector: fsm => fsm.States,
+                    persistor: (pn, places) => pn.Places.AddRange(places));
+            }
+        }
+
+        /// <summary>
+        /// This transformation rule transforms state machine transitions to transitions of the Petri net
+        /// </summary>
+        public class TransitionToTransition : TransformationRule<ITransition, PN.Transition>
+        {
+            /// <summary>
+            /// In the transformation method, we copy the input and register the created transition at the petri net
+            /// </summary>
+            /// <param name="input">The input state machine transition</param>
+            /// <param name="output">The target Petri net transition</param>
+            /// <param name="context">The context of this transformation run, used e.g. for tracing</param>
+            public override void Transform(ITransition input, PN.Transition output, ITransformationContext context)
+            {
+                output.Input = input.Input;
+
+                // We can always navigate to an elements container, even if there is no explicit reference
+                var fsm = input.Source.Parent as StateMachine;
+                // Here, we resolve the petri net created for the state machine containing the source state
+                var pn = context.Trace.ResolveIn(Rule<AutomataToNet>(), fsm);
+                if (pn != null)
+                {
+                    pn.Transitions.Add(output);
+                }
+            }
+
+            /// <summary>
+            /// Registration of dependencies
+            /// </summary>
+            public override void RegisterDependencies()
+            {
+                // First, we make sure that Transition2Transition is called each time a state is transformed
+                CallForEach(Rule<StateToPlace>(),
+                    selector: s => s.Outgoing,
+                    persistor: (p, transitions) => p.Outgoing.AddRange(transitions));
+
+                // Here, we make sure that the target state is also transformed before the transition is transformed
+                Require(Rule<StateToPlace>(),
+                    selector: t => t.Target,
+                    persistor: (t, p) => t.To.Add(p));
+            }
+        }
+
+        /// <summary>
+        /// This rule transforms each start state to a corresponding transition
+        /// </summary>
+        public class StartStateToTransition : TransformationRule<IState, PN.Transition>
+        {
+            /// <summary>
+            /// In the transform method, we add the corresponding place to the transition
+            /// </summary>
+            /// <param name="input">The input state</param>
+            /// <param name="output">The output transition</param>
+            /// <param name="context">The context of this transformation run, used e.g. for tracing</param>
+            public override void Transform(IState input, PN.Transition output, ITransformationContext context)
+            {
+                output.To.Add(context.Trace.ResolveIn(Rule<StateToPlace>(), input));
+                output.Input = "";
+            }
+
+            /// <summary>
+            /// Here we register when this transformation rule should be called
+            /// </summary>
+            public override void RegisterDependencies()
+            {
+                // We only need to call this transformation rule for the start state of the state machine
+                CallFor(Rule<AutomataToNet>(),
+                    selector: fsm => fsm.StartState,
+                    persistor: (t, pn) => pn.Transitions.Add(t));
+            }
+        }
+
+        /// <summary>
+        /// This transformation rule creates transitions for each final state
+        /// </summary>
+        public class EndStateToTransition : TransformationRule<IState, PN.Transition>
+        {
+            /// <summary>
+            /// Like in the start state transformation rule, we simply trace the place for which the transition has been created
+            /// </summary>
+            /// <param name="input">The input state</param>
+            /// <param name="output">The target transition</param>
+            /// <param name="context">The context of this transformation run, used e.g. for tracing</param>
+            public override void Transform(IState input, PN.Transition output, ITransformationContext context)
+            {
+                output.From.Add(context.Trace.ResolveIn(Rule<StateToPlace>(), input));
+                output.Input = "";
+            }
+
+            /// <summary>
+            /// Here, we register when this transformation rule should be called.
+            /// </summary>
+            public override void RegisterDependencies()
+            {
+                // The transformation rule should be called for every state that is accepting.
+                // Therefore, you can specify different guards for different situations for which the transformation rule should be called
+                CallForEach(Rule<AutomataToNet>(),
+                    selector: fsm => fsm.States.Where(s => s.IsAccepting),
+                    persistor: (pn, endTransitions) => pn.Transitions.AddRange(endTransitions));
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NMF.Models.Repository;
+using NMF.Expressions.Linq;
+using NMF.Transformations;
+
+using NMFDemo.Metamodels.FSM;
+using PN = NMFDemo.Metamodels.PN;
+
+namespace NMFDemo
+{
+    /// <summary>
+    /// This is a very small demonstration application that gives you a rough overview of what you can do with the .NET Modelling Framework.
+    /// For more information, please refer to http://nmf.codeplex.com or http://github.com/NMFCode/NMF
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Loading models
+
+            // At first, we create a new repository in which we want to load models
+            var repository = new ModelRepository();
+            // To actually load models into that repository, we simply need to resolve a URI. A locator component in the repository
+            // will handle different URI types. By default, file URIs are supported.
+            // The serializer automatically detects the assembly-wide attributes which we used to register the metamodel.
+            var model = repository.Resolve("Example.fsm");
+            // We can access the root elements and cast them into the model type
+            var fsm = model.RootElements[0] as StateMachine;
+
+            #endregion
+
+            #region Incremental model analysis
+
+            // To demonstrate incremental model analysis, we first create an analysis.
+            // For this we support LINQ-queries. Because we added a using statement to NMF.Expressions.Linq, 
+            // the stateHubs variable is of type IEnumerableExpression that offers both batch mode execution and incrementalization
+            var stateHubs = from s in fsm.States
+                            where s.Incoming.Count == fsm.States.Max(s2 => s2.Incoming.Count)
+                            select s.Name;
+
+            // Next, we switch to the incrementalized version using AsNotifiable() which offers an event CollectionChanged. We subscribe
+            // to this event and simply print any new elements to the console.
+            var incrementalStateHubs = stateHubs.AsNotifiable();
+            incrementalStateHubs.CollectionChanged += (o, e) =>
+            {
+                if (e.NewItems != null) foreach (string name in e.NewItems) Console.WriteLine("{0} is a new hub", name);
+                if (e.OldItems != null) foreach (string name in e.OldItems) Console.WriteLine("{0} is no longer a hub", name);
+            };
+
+            // To demonstrate how the event is fired, we now modify the model that we analysed for hubs before and insert a new transition.
+            var checkStock = fsm.States[1];
+            checkStock.Outgoing.Add(new Transition()
+            {
+                Input = "items are for free",
+                Target = fsm.States[2]
+            });
+
+            // Because we set the target of the new transition to an existing state, the transition is implicitly added to the set of incoming transitions
+            // for that state, because the Target reference of a transition is the opposite of a states Incoming reference.
+            // However, now the state Ship Order also has equally many incoming transitions than the maximum so far.
+            // As we switched on incremental analysis, the notifications created for our changes to the model have actually updated the analysis result
+            // Thus, if you insert a breakpoint here, you should actually see a message printed to the console, though we have not actively rerun the analysis.
+            // If we do not want to receive these updates anymore, there is also a Detach method available
+            incrementalStateHubs.Detach();
+            #endregion
+
+            #region Model transformation
+
+            // Now, we are going to run a model transformation on the finite state machine.
+            // For this, we just have to instantiate the model transformation and pass it to the transformation engine
+            var net = TransformationEngine.Transform<StateMachine, PN.Net>(fsm, new FSM2PN());
+            
+            #endregion
+
+            #region Saving models
+
+            // The easiest way to save a model physically to a file is to simply save it into a repository. This will persist the model as XMI file.
+            repository.Save(net, "Example.pn");
+
+            #endregion
+        }
+    }
+}
